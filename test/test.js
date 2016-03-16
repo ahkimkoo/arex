0c0208ded5b0c621f5d17d2cd69620b584ba4848
Cherokee <successage@gmail.com>
2016-03-16 19:11:50 +0800

    title, pubdate extraction

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var arex = require('../lib/arex.js');
 
 if(process.argv.length>2){
 	arex.get_article(process.argv[2],(err,result)=>{
-		console.log(result['content']);
+		console.log(result);
 	});
 }else console.log('useage: node test.js [link]');
