a271301690aaac9a2408c39937aac009224902fa
Cherokee <successage@gmail.com>
2016-03-29 11:03:13 +0800

    summarization supported

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var arex = require('../lib/arex.js');
 
 if(process.argv.length>2){
-	arex.get_article(process.argv[2],(err,result)=>{
+	arex.get_article(process.argv[2],process.argv[3]?parseInt(process.argv[3]):200,(err,result)=>{
 		console.log(result);
 	});
 }else console.log('useage: node test.js [link]');
